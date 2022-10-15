3262630b7d66b2ca534988da3c7c905f55d7e473
eglushenko <eglushenko@ptsecurity.com>
2022-10-15 12:40:27 +0500

    app

---
+++ demo/demo_app/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+
+class DemoAppTests(TestCase):
+    def setUp(self):
+        pass
+    
+    def test_demo_app_one(self):
+        pass

d52f44413454e205a02fd4bc2aec1a737a804a63
eglushenko <eglushenko@ptsecurity.com>
2022-10-15 12:31:53 +0500

    default

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9aw%d9_22kee$@g9yeo9rzsoicn7!d%q-7710k4)cupo+a@e9&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
